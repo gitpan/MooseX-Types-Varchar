58aca200afacae6ab418a613940b734856ed1731
Chris Andrews <chrisa@cpan.org>
2010-06-19 11:47:12 +0000

    Import of CHRISA/MooseX-Types-Varchar-0.02 from CPAN.
    gitpan-cpan-distribution: MooseX-Types-Varchar
    gitpan-cpan-version:      0.02
    gitpan-cpan-path:         CHRISA/MooseX-Types-Varchar-0.02.tar.gz
    gitpan-cpan-author:       CHRISA
    gitpan-cpan-maturity:     released

--- lib/MooseX/Types/Varchar.pm
+++ lib/MooseX/Types/Varchar.pm
@@ -3,44 +3,38 @@ use strict;
 use warnings;
 
 use 5.008;
-our $VERSION = '0.01';
+our $VERSION = '0.02';
 
 use MooseX::Types -declare => ['Varchar'];
 
 use Moose::Meta::TypeConstraint::Parameterized;
-use Moose::Util::TypeConstraints qw/ register_type_constraint
-                                     find_type_constraint /;
+use Moose::Util::TypeConstraints;
+use MooseX::Types::Moose qw/ Str Int /;
+use namespace::clean;
 
 use base qw/ Moose::Meta::TypeConstraint::Parameterizable /;
 
-sub parameterize {
-        my ($self, $length) = @_;
-        my $contained_tc = $self->_parse_type_parameter($length);
+sub _parse_type_parameter {
+    my ($self, $type_parameter) = @_;
+    Moose->throw_error(qq{Type parameter '$type_parameter' for Varchar is not an Int})
+        unless is_Int($type_parameter);
 
-        my $tc_name = $self->name . '[' . $length . ']';
-        my $parameterized = Moose::Meta::TypeConstraint::Parameterized->new(
-            name           => $tc_name,
-            parent         => $self,
-            type_parameter => $contained_tc,
-            message        => sub {
-                    my $value = shift;
-                    return "'$value' is too long for attribute type Varchar[$length]"
-            },
-        );
-        return $parameterized;
+    return type $type_parameter, # Evil, this shits a type called '20' which checks $_ < 20 into the global
+                                 # type constraint registry.
+        where { length($_) <= $type_parameter};
 }
 
-my $tc = MooseX::Types::Varchar->new(
+my $tc = __PACKAGE__->new(
         name                 => Varchar,
         package_defined_in   => __PACKAGE__,
-        parent               => find_type_constraint('Str'),
+        parent               => find_type_constraint( Str ),
         constraint           => sub {1},
         constraint_generator => sub {
-                my $length = shift;
+                my $type_parameter = shift;
+                my $check          = $type_parameter->_compiled_type_constraint;
                 return sub {
-                        return 1 if defined $_ && length $_ <= $length;
-                        return;
-                }
+                        return $check->($_);
+                };
         },
 );
 
@@ -69,20 +63,12 @@ MooseX::Types::Varchar - Str type parameterizable by length.
 =head1 DESCRIPTION
 
 This module provides a type based on Str, where a length restriction
-is paramterizable. You get a customised message indicating that there
-is a length restriction on the attribute:
-
-  '... long string ...' is too long for attribute type Varchar[20]
+is paramterizable.
 
 =head1 EXPORTS
 
 Nothing by default. You will want to request "Varchar", provided as a
 MooseX::Types type.
-
-=head1 BUGS
-
-You can't really usefully subtype these. If you do the constraint will be
-applied to the subtype, but the message will not. 
 
 =head1 AUTHOR
 
@@ -94,3 +80,4 @@ This program is Free software, you may redistribute it under the same
 terms as Perl itself.
 
 =cut
+
